42ab9d07da7562a8c59abe6b2cdd48866a1be8dc
Catalin Sanda <catalin.sanda@gmail.com>
2023-08-01 22:44:31 +0300

    Additional fix for issue #10  - Add support for version, time & datetype value types

---
+++ src/main/java/org/openhab/binding/solarman/internal/util/StreamUtils.java
@@ -0,0 +1,57 @@
+package org.openhab.binding.solarman.internal.util;
+
+import java.util.*;
+import java.util.function.BiFunction;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class StreamUtils {
+    public static <A, B, C> Stream<C> zip(Stream<? extends A> a,
+                                          Stream<? extends B> b,
+                                          BiFunction<? super A, ? super B, ? extends C> zipper) {
+        Objects.requireNonNull(zipper);
+        Spliterator<? extends A> aSpliterator = Objects.requireNonNull(a).spliterator();
+        Spliterator<? extends B> bSpliterator = Objects.requireNonNull(b).spliterator();
+
+        // Zipping looses DISTINCT and SORTED characteristics
+        int characteristics = aSpliterator.characteristics() & bSpliterator.characteristics() &
+                ~(Spliterator.DISTINCT | Spliterator.SORTED);
+
+        long zipSize = ((characteristics & Spliterator.SIZED) != 0)
+                ? Math.min(aSpliterator.getExactSizeIfKnown(), bSpliterator.getExactSizeIfKnown())
+                : -1;
+
+        Iterator<A> aIterator = Spliterators.iterator(aSpliterator);
+        Iterator<B> bIterator = Spliterators.iterator(bSpliterator);
+        Iterator<C> cIterator = new Iterator<C>() {
+            @Override
+            public boolean hasNext() {
+                return aIterator.hasNext() && bIterator.hasNext();
+            }
+
+            @Override
+            public C next() {
+                return zipper.apply(aIterator.next(), bIterator.next());
+            }
+        };
+
+        Spliterator<C> split = Spliterators.spliterator(cIterator, zipSize, characteristics);
+        return (a.isParallel() || b.isParallel())
+                ? StreamSupport.stream(split, true)
+                : StreamSupport.stream(split, false);
+    }
+
+    public static Collection<Object> reverse(List<Integer> list) {
+        return list.stream().reduce(new ArrayList<>(), (l, i) -> {
+            l.add(0, i);
+            return l;
+        }, (l1, l2) -> {
+            l2.addAll(l1);
+            return l2;
+        });
+    }
+
+
+    public record Tuple<A, B>(A a, B b) {
+    }
+}
